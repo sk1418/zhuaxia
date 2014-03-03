a0b590087bc8c11be39f02618eea5cb7b26f49b4
Kai Yuan <kent.yuan@Gmail.com>
2014-03-03 23:50:11 +0100

    add xiami class with login impl. (cookie)

--- zhuaxia/xiami.py
+++ zhuaxia/xiami.py
@@ -1,71 +1,95 @@
 # -*- coding:utf-8 -*-
-
-####################################################################
-# taken from https://gist.github.com/lepture/1014329
-####################################################################
-#
-# Copyright (c) 2011, lepture.com
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#    * Redistributions of source code must retain the above copyright
-#      notice, this list of conditions and the following disclaimer.
-#    * Redistributions in binary form must reproduce the above
-#      copyright notice, this list of conditions and the following
-#      disclaimer in the documentation and/or other materials provided
-#      with the distribution.
-#    * Neither the name of the author nor the names of its contributors
-#      may be used to endorse or promote products derived from this
-#      software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-# HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
+import time
 import urllib
 import httplib
 from contextlib import closing
 from Cookie import SimpleCookie
+from os import path
 
-ua = 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36'
-#ua = 'Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19'
+#xiami android/iphone api urls
+url_xiami="http://www.xiami.com"
+url_song = "http://www.xiami.com/app/android/song?id=%s"
+url_album = "http://www.xiami.com/app/android/album?id=%s"
+url_collect = "http://www.xiami.com/app/android/collect?id=%s"
+url_artist_albums = "http://www.xiami.com/app/android/artist-albums?id=%s&page=%s"
+url_artist_top_song = "http://www.xiami.com/app/android/artist-topsongs?id=%s"
+url_lib_songs = "http://www.xiami.com/app/android/lib-songs?uid=%s&page=%s"
+
+#agent string for http request header
+AGENT= 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36'
+
+
+class Song(object):
+    """xiami Song class"""
+    def init(self,url):
+        self.url = url
+
+        
+class Album(object):
+    """The xiami album object"""
+    def __init__(self, url):
+        self.url = url 
+        self.album_id = album_id
+
+class Favorite(object):
+    def __init__(self,url):
+        self.url = url
+
+
 
 checkin_headers = {
-    'User-Agent': ua,
+    'User-Agent': AGENT,
     'Content-Length': '0',
     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
     'X-Requested-With': 'XMLHttpRequest',
     'Host': 'www.xiami.com',
-    'Origin': 'http://www.xiami.com/',
-    'Referer': 'http://www.xiami.com/',
+    'Origin': url_xiami,
+    'Referer': url_xiami,
     'Content-Length': '0',
 }
 
-class xiami_login(object):
-    def __init__(self, email, password):
+class Xiami(object):
+    def __init__(self, email, password, cookie_file):
         self.email = email
         self.password = password
         self._auth = None
+        self.cookie_file = cookie_file
+        self.member_auth = ""
+
+    def login_with_cookie(self):
+        ts = str(int(time.time()))
+        cookie_member_auth = None
+        if path.exists(self.cookie_file):
+            print 'read member_auth from cookie file ...'
+            with open(self.cookie_file) as f:
+                cif = f.read().split(' ')
+                tm_valid = (int(ts) - int(cif[0])) > 18000 
+                self._auth = cif[1]
+                cookie_member_auth = self._auth
+                auth = self.checkin()
+                if auth != '0' or tm_valid:
+                    cookie_member_auth = self.write_cookie(ts)
+        else:
+           cookie_member_auth = self.write_cookie(ts)
+        self.member_auth = cookie_member_auth
+
+    def write_cookie(self, ts):
+        cookie_auth = self.login()
+        print 'Writing cookie file ...'
+        with open(self.cookie_file, 'w') as f:
+            f.write(ts + ' ' + cookie_auth)
+        return cookie_auth
+
 
     def login(self):
-        print 'login ....'
+        print 'login with email and password....'
         _form = {
             'email': self.email,
             'password': self.password,
             'LoginButton': '登录',
         }
         data = urllib.urlencode(_form)
-        headers = {'User-Agent': ua}
+        headers = {'User-Agent': AGENT}
         headers['Referer'] = 'http://www.xiami.com/web/login'
         headers['Content-Type'] = 'application/x-www-form-urlencoded'
         with closing(httplib.HTTPConnection('www.xiami.com')) as conn:
@@ -86,4 +110,6 @@ class xiami_login(object):
             res = conn.getresponse()
             return res.read()
 
-####################################################################
+if __name__ == '__main__':
+    xm = Xiami('kent.yuan@gmail.com', '######','/tmp/test/xm.cookie')
+    xm.login_with_cookie();
