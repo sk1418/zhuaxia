7a2efe1b9e4a71220bddb642e04c9690032c5a87
Kent@LsyHP <kent.yuan@Gmail.com>
2014-03-26 18:33:23 +0100

    add commander.py

---
+++ zhuaxia/commander.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+
+import sys
+import config ,util ,logging ,log,downloader
+import xiami as xm
+
+LOG = log.get_logger("zxLogger")
+
+
+def from_file(file):
+    """ download objects (songs, albums...) from an input file.  """
+    pass
+
+
+def parse_and_dl(input):
+    """ parse the input string (xiami url), and do download"""
+    pass
