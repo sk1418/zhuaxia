8d00dc6cca46134edcb5323fe3a4b43f86b286ed
Kent@LsyHP <kent.yuan@Gmail.com>
2014-04-08 17:33:57 +0200

    add zx -v to show version info

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import glob
+setup(
+    name = 'zhuaxia',
+    version = '1.0.0',
+    install_requires=[ 'requests','mutagen' ],
+    packages = find_packages(),
+    package_data={'zhuaxia':['conf/*.conf']},
+    #data_files=[('conf',glob.glob('conf/*.*'))],
+    include_package_data= True,
+    zip_safe=False,
+    scripts=['zx'],
+    author='Kai Yuan',
+    author_email='kent.yuan@gmail.com',
+    platforms=['POSIX'],
+    keywords='xiami mp3 download',
+    url='https://github.com/sk1418/zhuaxia',
+    description='a cli tool to download mp3 from xiami.com',
+    long_description="""
+        a cli tool to download mp3 from xiami.com
+    """,
+)
